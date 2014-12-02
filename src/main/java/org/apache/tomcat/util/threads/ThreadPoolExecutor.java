173bd924ed85c8730dcded5938d6350bd8598b69
Bill <bert825_work@163.com>
2014-12-02 15:33:55 +0800

    [Core] ThreadPoolExecutor - 与JDK的ThreadPoolExecutor实现同样的功能 (因为直接继承自它)，但实现了一个更高效的getSubmittedCount()方法，用于处理任务工作队列
    void execute(Runnable command, long timeout, TimeUnit
    unit)：在未来的某一时刻，执行给定的命令。
    命令可能是在一个新的线程中执行，也可能在一个连接池的线程中执行，或者在调用线程中执行。
    如果没有线程可用，它会被插入到任务工作队列中。如果任务工作队列满了，系统会等待一定的时间。在等待这段时间后，如果队列仍然是满的，就会抛出一个"队列容量满了"的拒绝执行异常。

--- src/main/java/org/apache/tomcat/util/threads/ThreadPoolExecutor.java
+++ src/main/java/org/apache/tomcat/util/threads/ThreadPoolExecutor.java
@@ -29,19 +29,22 @@ import org.apache.tomcat.util.res.StringManager;
 /**
  * Same as a java.util.concurrent.ThreadPoolExecutor but implements a much more efficient
  * {@link #getSubmittedCount()} method, to be used to properly handle the work queue.
- * If a RejectedExecutionHandler is not specified a default one will be configured
+ * 
+ * <p>If a RejectedExecutionHandler is not specified a default one will be configured
  * and that one will always throw a RejectedExecutionException
+ * <p>
+ * 与JDK的{@link java.util.concurrent.ThreadPoolExecutor ThreadPoolExecutor}实现同样的功能
+ * (因为直接继承自它)，但实现了一个更高效的{@link #getSubmittedCount()}方法，用于处理任务工作队列。
+ * 
  * @author fhanik
- *
  */
 public class ThreadPoolExecutor extends java.util.concurrent.ThreadPoolExecutor {
-    /**
-     * The string manager for this package.
-     */
-    protected static final StringManager sm = StringManager
-            .getManager("org.apache.tomcat.util.threads.res");
+
+	protected static final StringManager sm = StringManager.getManager("org.apache.tomcat.util.threads.res");
 
     /**
+     * 已提交但未完成的任务数，其包括队列中的任务和已被工作者线程处理但还未开始执行的任务。
+     * <p>
      * The number of tasks submitted but not yet finished. This includes tasks
      * in the queue and tasks that have been handed to a worker thread but the
      * latter did not start executing the task yet.
@@ -62,19 +65,59 @@ public class ThreadPoolExecutor extends java.util.concurrent.ThreadPoolExecutor 
      */
     private long threadRenewalDelay = Constants.DEFAULT_THREAD_RENEWAL_DELAY;
 
+    /**
+     * 创建一个Tomcat线程池执行器。
+     * 
+     * @param corePoolSize 核心线程数
+     * @param maximumPoolSize 线程池持有的最大线程数
+     * @param keepAliveTime 存活时间
+     * @param unit
+     * @param workQueue 任务工作队列
+     * @param handler 拒绝执行处理器
+     */
     public ThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, RejectedExecutionHandler handler) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
     }
 
+    /**
+     * 创建一个Tomcat线程池执行器。
+     * 
+     * @param corePoolSize
+     * @param maximumPoolSize
+     * @param keepAliveTime
+     * @param unit
+     * @param workQueue
+     * @param threadFactory 线程工厂
+     * @param handler
+     */
     public ThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory,
             RejectedExecutionHandler handler) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
     }
 
+    /**
+     * 创建一个基于拒绝处理器的Tomcat线程池执行器。
+     * 
+     * @param corePoolSize
+     * @param maximumPoolSize
+     * @param keepAliveTime
+     * @param unit
+     * @param workQueue
+     * @param threadFactory
+     */
     public ThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue, ThreadFactory threadFactory) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, new RejectHandler());
     }
 
+    /**
+     * 创建一个基于拒绝处理器的Tomcat线程池执行器。
+     * 
+     * @param corePoolSize
+     * @param maximumPoolSize
+     * @param keepAliveTime
+     * @param unit
+     * @param workQueue
+     */
     public ThreadPoolExecutor(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, new RejectHandler());
     }
@@ -130,6 +173,11 @@ public class ThreadPoolExecutor extends java.util.concurrent.ThreadPoolExecutor 
         return false;
     }
 
+    /**
+     * 返回已提交但未完成的任务数。
+     * 
+     * @return
+     */
     public int getSubmittedCount() {
         return submittedCount.get();
     }
@@ -143,9 +191,17 @@ public class ThreadPoolExecutor extends java.util.concurrent.ThreadPoolExecutor 
     }
 
     /**
+     * 在未来的某一时刻，执行给定的命令。<br>
+     * 命令可能是在一个新的线程中执行，也可能在一个连接池的线程中执行，或者在调用线程中执行。
+     * <p>
+     * 如果没有线程可用，它会被插入到任务工作队列中。<br>
+     * 如果任务工作队列满了，系统会等待一定的时间。在等待这段时间后，
+     * 如果队列仍然是满的，就会抛出一个"被拒绝执行的异常"。
+     * <p>
      * Executes the given command at some time in the future.  The command
      * may execute in a new thread, in a pooled thread, or in the calling
      * thread, at the discretion of the <tt>Executor</tt> implementation.
+     * 
      * If no threads are available, it will be added to the work queue.
      * If the work queue is full, the system will wait for the specified
      * time and it throw a RejectedExecutionException if the queue is still
@@ -160,16 +216,18 @@ public class ThreadPoolExecutor extends java.util.concurrent.ThreadPoolExecutor 
         submittedCount.incrementAndGet();
         try {
             super.execute(command);
-        } catch (RejectedExecutionException rx) {
-            if (super.getQueue() instanceof TaskQueue) {
-                final TaskQueue queue = (TaskQueue)super.getQueue();
+        } catch (RejectedExecutionException rx) { // 第一次被拒绝执行
+            if (super.getQueue() instanceof TaskQueue) { // 如果工作队列是任务队列
+                final TaskQueue queue = (TaskQueue) super.getQueue();
                 try {
-                    if (!queue.force(command, timeout, unit)) {
+                    if (!queue.force(command, timeout, unit)) { // 能否强制将任务插入到阻塞队列中
+                    	// 强制插入失败，抛出"队列容量满了"的拒绝执行异常！
                         submittedCount.decrementAndGet();
                         throw new RejectedExecutionException("Queue capacity is full.");
                     }
-                } catch (InterruptedException x) {
+                } catch (InterruptedException x) {// 出现"线程中断"异常
                     submittedCount.decrementAndGet();
+                    // 标记线程中断状态
                     Thread.interrupted();
                     throw new RejectedExecutionException(x);
                 }
@@ -210,14 +268,18 @@ public class ThreadPoolExecutor extends java.util.concurrent.ThreadPoolExecutor 
         this.setCorePoolSize(savedCorePoolSize);
     }
 
+    /**
+     * 拒绝处理器
+     */
     private static class RejectHandler implements RejectedExecutionHandler {
+
         @Override
         public void rejectedExecution(Runnable r,
                 java.util.concurrent.ThreadPoolExecutor executor) {
+        	// 抛出"拒绝执行异常"
             throw new RejectedExecutionException();
         }
 
     }
-
 
 }
